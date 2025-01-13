6969bc379d4cd904a50faa1c159b10100cf10b42
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2025-01-13 18:30:17 +0100

    import firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
+import app from './firebase';
+// Style
 import './App.css';
 
-function App() {
+export default function App() {
+  console.log("Firebase app initialized:", app);
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Kanban App</h1>
       </header>
     </div>
   );
 }
-
-export default App;
