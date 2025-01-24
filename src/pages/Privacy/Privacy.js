51fb12513add0792d901105b5336fff932ea5bd4
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2025-01-24 08:06:00 +0100

    content privacy page

--- src/pages/Privacy/Privacy.js
+++ src/pages/Privacy/Privacy.js
@@ -6,8 +6,16 @@ import './Privacy.css'
 
 export default function Privacy() {
   return (
-    <div>
-      <p>Privacy</p>
+    <div className='workspace'>
+      <h1>Privacy</h1>
+      <div className='section'>
+        <h2>Subtitle</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.</p>
+      </div>
+      <div className='section'>
+        <h2>Subtitle</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.</p>
+      </div>
     </div>
   )
 }
